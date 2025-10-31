428793ba9ead68f7ee6e591e23582f5bfd7d8014
Tran Hong Duc <tranhongduc_t67@hus.edu.vn>
2025-10-31 14:50:13 +0700

    Remove apps, docker, infra

--- apps/frontend/src/config/mapConfig.js
+++
@@ -1,9 +0,0 @@
-import L from 'leaflet';
-
-export const SOCKET_URL = 'http://localhost:4000';
-export const ASPECT_RATIO = 360 / 170.1;
-export const GRID_HEIGHT = 2000;
-export const GRID_WIDTH = Math.round(GRID_HEIGHT * ASPECT_RATIO);
-export const WORLD_BOUNDS = L.latLngBounds([-85.05112878, -180], [85.05112878, 180]);
-export const CHUNK_SIZE = 256;
-export const MIN_ZOOM_TO_SHOW_PIXELS = 8;
--- apps/map-service/src/config/constants.ts
+++
@@ -1,2 +0,0 @@
-export const CHUNK_SIZE = 256;
-export const PORT = process.env.PORT || 4000;
