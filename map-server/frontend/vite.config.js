a2b949804e26b5efcb442e6f718bb2a38aba79f5
Hmmmz <155872648+5thAugust@users.noreply.github.com>
2025-12-02 09:47:05 +0700

    Update vite.config.js

--- map-server/frontend/vite.config.js
+++ map-server/frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Khi frontend (chạy trên port 5173) gửi request đến '/api',
+      // Vite dev server sẽ chuyển tiếp nó đến backend (chạy trên port 4000).
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+      // Cấu hình tương tự cho Socket.IO
+      '/socket.io': {
+        target: 'ws://localhost:4000', // Sử dụng websocket protocol
+        ws: true,
+      }
+    }
+  }
 })
